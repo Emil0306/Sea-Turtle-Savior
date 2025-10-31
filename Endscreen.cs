fd36b96fafb3f9b6f1d0650cac9043e5f3b9ded9
Emil Nielsen <emil0306dk@gmail.com>
2025-10-31 10:57:45 +0100

    Endscreen file made + empty methods

---
+++ Endscreen.cs
@@ -0,0 +1,21 @@
+class EndScreen{
+	// Atributes
+	private double endtime;
+	private double pollutionprocent;
+	private double machineProgress;
+
+	// Constructors
+	public EndScreen (double endtime, double pollutionprocent, double cleaningMachineProgress) {
+		this.endtime = time; 										// Get current time from "Spiller"
+		this.pollutionprocent = pollution; 							// Get pollution from "Forureningmeter"
+		this.cleaningMachineProgress = cleaningMachineProgress;		// Get cleaning machine progress from "Rensemaskine"
+	}
+
+	// Methods
+	public void EndInfo () {	// Display an endscreen with time, pollution and the cleaning machine progress
+
+	}
+	public void Restart () {	// Restart game to be played again
+
+	}
+}
