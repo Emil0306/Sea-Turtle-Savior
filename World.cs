0e7c9abd7ebf92d476fe5113a28b663bbd607b84
Emil Nielsen <emil0306dk@gmail.com>
2025-10-31 23:15:50 +0100

    Added harbor exit in every room

--- World.cs
+++ World.cs
@@ -20,34 +20,42 @@ class World {
     
     harbor.AddEdge("east", cleaningMachine);
     harbor.AddEdge("west", wasteStation);
-    cleaningMachine.AddEdge("harbor", harbor);
-    wasteStation.AddEdge("harbor", harbor);
+    cleaningMachine.AddEdge("harbor", harbor);  // Delete when add harbor from everywhere
+    wasteStation.AddEdge("harbor", harbor);     // Delete when add harbor from everywhere
     harbor.AddEdge("north", water2);
     water1.AddEdge("east", water2);
     water1.AddEdge("north", water4);
-    water2.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
+    water1.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water2.AddEdge("east", water3);
     water2.AddEdge("west", water1);
     water2.AddEdge("north", water5);
+    water2.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water3.AddEdge("west", water2);
     water3.AddEdge("north", water6);
+    water3.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water4.AddEdge("east", water5);
     water4.AddEdge("north", water7);
     water4.AddEdge("south", water1);
+    water4.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water5.AddEdge("north", water7);
     water5.AddEdge("west", water4);
     water5.AddEdge("east", water6);
     water5.AddEdge("south", water2);
+    water5.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water6.AddEdge("west", water5);
     water6.AddEdge("north", water9);
     water6.AddEdge("south", water3);
+    water6.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water7.AddEdge("south", water4);
     water7.AddEdge("east", water8);
+    water7.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water8.AddEdge("east", water9);
     water8.AddEdge("west", water7);
     water8.AddEdge("south", water5);
+    water8.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
     water9.AddEdge("south", water6);
     water9.AddEdge("west", water8);
+    water9.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
 
 
     this.entry = harbor;
