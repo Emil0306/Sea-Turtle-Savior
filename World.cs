f0ab42675f7b9c6d1a4276236de8e292124f1714
Emil Nielsen <emil0306dk@gmail.com>
2025-10-31 20:28:16 +0100

    3x3 grid + harbor + cleaning machine in World.cs

--- World.cs
+++ World.cs
@@ -5,20 +5,52 @@ class World {
   Space entry;
   
   public World () {
-    Space entry    = new Space("Entry");
-    Space corridor = new Space("Corridor");
-    Space cave     = new Space("Cave");
-    Space pit      = new Space("Darkest Pit");
-    Space outside  = new Space("Outside");
+    Space harbor          = new Space("Harbor");
+    Space cleaningMachine = new Space("Cleaning Machine");
+    Space wasteStation    = new Space("WasteStation");
+    Space water1          = new Space("Water1");
+    Space water2          = new Space("Water2");
+    Space water3          = new Space("Water3");
+    Space water4          = new Space("Water4");
+    Space water5          = new Space("Water5");
+    Space water6          = new Space("Water6");
+    Space water7          = new Space("Water7");
+    Space water8          = new Space("Water8");
+    Space water9          = new Space("Water9");
     
-    entry.AddEdge("door", corridor);
-    corridor.AddEdge("door", cave);
-    cave.AddEdge("north", pit);
-    cave.AddEdge("south", outside);
-    pit.AddEdge("door", cave);
-    outside.AddEdge("door", cave);
-    
-    this.entry = entry;
+    harbor.AddEdge("east", cleaningMachine);
+    harbor.AddEdge("west", wasteStation);
+    cleaningMachine.AddEdge("harbor", harbor);
+    wasteStation.AddEdge("harbor", harbor);
+    harbor.AddEdge("north", water2);
+    water1.AddEdge("east", water2);
+    water1.AddEdge("north", water4);
+    water2.AddEdge("harbor", harbor);   // Delete when add harbor from everywhere
+    water2.AddEdge("east", water3);
+    water2.AddEdge("west", water1);
+    water2.AddEdge("north", water5);
+    water3.AddEdge("west", water2);
+    water3.AddEdge("north", water6);
+    water4.AddEdge("east", water5);
+    water4.AddEdge("north", water7);
+    water4.AddEdge("south", water1);
+    water5.AddEdge("north", water7);
+    water5.AddEdge("west", water4);
+    water5.AddEdge("east", water6);
+    water5.AddEdge("south", water2);
+    water6.AddEdge("west", water5);
+    water6.AddEdge("north", water9);
+    water6.AddEdge("south", water3);
+    water7.AddEdge("south", water4);
+    water7.AddEdge("east", water8);
+    water8.AddEdge("east", water9);
+    water8.AddEdge("west", water7);
+    water8.AddEdge("south", water5);
+    water9.AddEdge("south", water6);
+    water9.AddEdge("west", water8);
+
+
+    this.entry = harbor;
   }
   
   public Space GetEntry () {
