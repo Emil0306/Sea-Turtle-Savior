333d1eff94bc870aa4887ace009e5ddf1e903bf3
NoahBR <noah.b.rasmussen@gmail.com>
2025-10-31 11:06:31 +0100

    Create Trash.cs

---
+++ Trash.cs
@@ -0,0 +1,16 @@
+class Trash {
+	private string name;
+	private string material;
+
+	public Car(string name, string material){
+		this.name = name;
+		this.material = material;
+	}
+
+	public string GetMaterial(){
+		return material;
+	}
+	public string GetName(){
+		return name;
+	}
+}
