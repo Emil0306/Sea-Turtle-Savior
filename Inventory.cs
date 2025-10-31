1f58552c2e6cd5dd74d8f0248e4b50843eb4d95b
NoahBR <noah.b.rasmussen@gmail.com>
2025-10-31 15:01:24 +0100

    Tilføjede Inventory Klassen
    Der skal laves et rum før denne klasse bliver fuldendt

---
+++ Inventory.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+
+class Inventory
+{
+	private List<Trash> inventoryList = new List<Trash>();
+	private int capacity = 15;
+
+	public void CollectTrash(Trash trash)
+	{
+		
+		if (inventoryList.Count < capacity)
+		{
+			inventoryList.Add(trash);
+			Console.WriteLine($"{trash.Name} added to inventory.");
+		}
+		else 
+        {
+            FullCapacity(); 
+        }
+			
+	}
+
+	public void RemoveTrash(int item)
+	{
+			inventoryList.Remove(inventoryList[item]); 
+	}
+
+	public void FullCapacity() // Player Should then die, but will be added later on.
+	{
+			Console.ForegroundColor = ConsoleColor.Red; 
+            Console.WriteLine("Inventory full! Cannot add more trash."); 
+        	Console.ForegroundColor = ConsoleColor.Gray; 
+	}
+
+
+
+	public void GetInventory()
+	{
+		Console.ForegroundColor = ConsoleColor.Green; 
+		Console.WriteLine("Inventory: ");
+		Console.ForegroundColor = ConsoleColor.Gray;
+
+		for (int i = 0 ; i < inventoryList.Count ; i++) 
+		{
+			Console.WriteLine($"{inventoryList[i].Name} af {inventoryList[i].Material}"); // Prints out the waste's name and material type.
+		}
+
+		
+	}
+
+	public static void Main()
+	{	
+		
+		Inventory myInv = new Inventory();
+		
+
+		myInv.CollectTrash(new Trash("Dildo", "Plastic"));
+		myInv.CollectTrash(new Trash("Heineken", "Plastic"));
+		myInv.CollectTrash(new Trash("Træ", "Plastic"));
+		myInv.CollectTrash(new Trash("Køleskab", "Plastic"));
+		myInv.CollectTrash(new Trash("Bue", "Plastic"));
+		myInv.CollectTrash(new Trash("Pil", "Plastic"));
+		myInv.CollectTrash(new Trash("Hjul", "Plastic"));
+		myInv.CollectTrash(new Trash("Deo", "Plastic"));
+		myInv.CollectTrash(new Trash("Jernrør", "Plastic"));
+		myInv.CollectTrash(new Trash("Hammer", "Plastic"));
+		myInv.CollectTrash(new Trash("Spyd", "Plastic"));
+		myInv.CollectTrash(new Trash("Malerspand", "Plastic"));
+		myInv.CollectTrash(new Trash("Tang", "Plastic"));
+		myInv.CollectTrash(new Trash("Sten", "Plastic"));
+		myInv.CollectTrash(new Trash("Iphone", "Plastic"));
+		myInv.CollectTrash(new Trash("Bil", "Plastic"));
+		myInv.GetInventory();
+
+		myInv.RemoveTrash(0);
+		myInv.GetInventory();
+
+	}
+
+
+}
