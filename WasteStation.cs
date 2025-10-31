a7554d30c8ba2dc501bbcaf3d5d15a17988097d1
NoahBR <noah.b.rasmussen@gmail.com>
2025-10-31 20:26:52 +0100

    Create WasteStation.cs

---
+++ WasteStation.cs
@@ -0,0 +1,18 @@
+// WasteStation class for sorting and recycling trash in different containers. 
+
+class WasteStation {
+	private 
+
+}
+
+
+class Container {
+
+	public string SortingType {get;}
+
+	public Container(string SortingType){
+		this.SortingType = SortingType;
+	}
+
+
+}
