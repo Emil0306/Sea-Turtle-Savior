e08d4166d6b1416a223a91859b0510212df9f43d
Sebastian <sebastianknudsen04@gmail.com>
2025-11-03 16:22:08 +0100

    Space.cs - visuelt billede af skrald

--- Space.cs
+++ Space.cs
@@ -24,7 +24,57 @@ class Space : Node {
     return (Space) (base.FollowEdge(direction));
   }
 
-  public string MakeMaps(HashSet<string> exits){ // to make variable positions make parameters like pos1 and pos 2
+  public string MakeMaps(HashSet<string> exits) {
+    int mapSize = 9;
+    string makemap = "";
+    if (exits.Contains("north")){
+        for (int i = 0 ; i<((mapSize*3)/2)+5 ; i++){
+            makemap += " ";
+        }
+        makemap += "north\n";
+    } else{
+        makemap += "\n";
+    }
+    makemap += Borders(mapSize);
+
+    for (int i=0 ; i<mapSize ; i++){
+      for (int h=0 ; h<mapSize ; h++){
+        if (exits.Contains("west") && h==0 && i==mapSize/2){
+            makemap += "west";
+        } else if (h==0){
+            makemap += "    ";
+        }
+
+        if (h==0) makemap += " | ";
+
+        if (i==4 && h==4) makemap += "🐢 ";
+
+        else if (i==3 && h==7) makemap += "🥤 ";
+
+        else makemap += " ~ ";
+
+        if (h==mapSize-1) makemap += " | ";
+
+       
+
+        if (exits.Contains("east") && i==mapSize/2 && h==mapSize-1) makemap += "east";
+      }
+      makemap += "\n";
+    }
+    makemap += Borders(mapSize);
+    if (exits.Contains("south")){
+        for (int i = 0 ; i<((mapSize*3)/2)+5 ; i++){
+            makemap += " ";
+        }
+        makemap += "south\n";
+    } else{
+        makemap += "\n";
+    }
+    return makemap;
+  }
+      
+  /* NEDENSTÅENDE ER FOR SKRALD I "VINDUET"
+  public string MakeMaps(HashSet<string> exits, int pos1, int pos2){ // to make variable positions make parameters like pos1 and pos 2
     int mapSize = 9;
     string makemap = "";
     if (exits.Contains("north")){
@@ -46,10 +96,15 @@ class Space : Node {
 
         if (h==0) makemap += " | ";
 
-        if (i==4 && h==4) makemap += "🐢 "; // to make variable positions: i == pos1 && h == pos2
+        if (i==4 && h==4) makemap += "🐢 ";
+
+        else if (i==pos1 && h==pos2) makemap += "🥤 ";
+
         else makemap += " ~ ";
 
         if (h==mapSize-1) makemap += " | ";
+
+       
 
         if (exits.Contains("east") && i==mapSize/2 && h==mapSize-1) makemap += "east";
       }
@@ -66,6 +121,18 @@ class Space : Node {
     }
     return makemap;
   }
+
+  private string Borders(int mapSize){
+    string border = "     ";
+    for (int i = 0 ; i<mapSize*3+4 ; i++){
+      border += "-";
+    }
+    border += "\n";
+    return border;
+  }
+}
+*/
+
 
   private string Borders(int mapSize){
     string border = "     ";
