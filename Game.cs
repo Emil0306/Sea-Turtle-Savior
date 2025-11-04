98ce19c4ae6fe5dc97175d6a83558a96e0f48432
ososm25 <osman1dk@hotmail.com>
2025-11-04 12:51:57 +0100

    Update Game.cs
    Game Class kode , der skal kontrolleres om det er rigitg

--- Game.cs
+++ Game.cs
@@ -1,32 +1,96 @@
 /* Main class for launching the game
  */
 
-class Game {
-  static World    world    = new World();
-  static Context  context  = new Context(world.GetEntry());
+class Game
+{
+  private bool IsRunning;
+  private int pollutionMeter = 0;
+
+  public void startStartScreen()
+  {
+    Start start = new Start();
+    start.Game();
+  }
+
+  public void startGame()
+  static void Main(string[] args)
+    {
+         Console.WriteLine("Welcome to Sea Turtle Savior!");
+
+    InitRegistry();
+    context.GetCurrent().Welcome();
+
+    }
+  
+  
+private int pollutionMeter = 0;
+  public void updatePollutionMeter()
+  {
+    pollutionMeter = +1;
+    Console.WriteLine($"pollution: {pollutionMeter}");
+  }
+  public void gamestatus()
+  {
+    if (pollutionMeter >= 100)
+    {
+      Console.WriteLine("The sea is too polluted");
+      endGame();
+    }
+  }
+  public void endGame()
+    {
+         while (context.IsRunning() )
+      {
+      Console.Write("> ");
+      string? line = Console.ReadLine();
+      if (line != null) registry.Dispatch(line);
+       Console.WriteLine("Game Over ");
+
+      }
+  
+    }
+   
+
+    
+  
+    
+    
+ 
+  static World world = new World();
+  static Context context = new Context(world.GetEntry());
   static ICommand fallback = new CommandUnknown();
   static Registry registry = new Registry(context, fallback);
-  
-  private static void InitRegistry () {
+
+  private static void InitRegistry()
+  {
     ICommand cmdExit = new CommandExit();
     registry.Register("exit", cmdExit);
     registry.Register("quit", cmdExit);
     registry.Register("bye", cmdExit);
     registry.Register("go", new CommandGo());
     registry.Register("help", new CommandHelp(registry));
-    registry.Register("collect", new CommandCollect());
   }
-  
-  static void Main (string[] args) {
-    Console.WriteLine("Welcome to the World of Zuul!");
+
+  static void Main(string[] args)
+  {
     
+    Console.WriteLine("Welcome to Sea Turtle Savior!");
+
     InitRegistry();
     context.GetCurrent().Welcome();
-    while (context.IsDone()==false) {
+
+    while (context.IsRunning() )
+    {
       Console.Write("> ");
       string? line = Console.ReadLine();
-      if (line!=null) registry.Dispatch(line);
+      if (line != null) registry.Dispatch(line);
     }
     Console.WriteLine("Game Over 😥");
   }
 }
+
+
+
+
+
+
