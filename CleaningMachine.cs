a7643430d8bc1cfeec90449283116562371b52e4
Ruahm25 <roccetgoald@gmail.com>
2025-11-04 10:27:56 +0100

    forsøg på CleaningMachine

---
+++ CleaningMachine.cs
@@ -0,0 +1,46 @@
+public class CleaningMachine
+{
+    private int progress = 0;
+
+    public int Progress
+    {
+        get => progress;
+        private set
+        {
+            if (value < 0) progress = 0;
+            else if (value > 100) progress = 100;
+            else progress = value;
+        }
+    }
+
+    private string[] constructionList=
+    {
+        "Strong metal body to hold everything together",
+        "Floating parts to help it stay on the water",
+        "Trash collector to scoop up plastic and metal",
+        "Filter to clean out dirty or chemical water",
+        "Storage boxes for the collected trash",
+        "Battery or solar power to keep it running",
+        "Simple control panel to steer and start it"
+    };
+
+    public int GetProgress()
+    {
+        return Progress;
+    }
+
+    public string[] GetConstructionList()
+    {
+        return constructionList;
+    }
+
+    public void ChangeProgress(int newProgress)
+    {
+        Progress=newProgress;
+    }
+
+    public void GoHarbor()
+    {
+        Console.WriteLine ("Going Harbor");
+    }
+}
