b9dcd96d21bcfe4d7c9bf1c3157259017f88204c
Sebastian <sebastianknudsen04@gmail.com>
2025-11-06 22:26:07 +0100

    Pollutionmeter visual - test

--- Pollutionmeter.cs
+++ Pollutionmeter.cs
@@ -33,3 +33,68 @@ class Pollutionmeter
         }
     }
 }
+
+
+class PollutionMeterVisual
+{
+    int pollution = 0;
+    int maxPollution = 100;
+
+    // Metode til når tiden går
+    public void IncreasePollution(int amount)
+    {
+        pollution = pollution + amount;
+        if (pollution > maxPollution)
+        {
+            pollution = maxPollution;
+        }
+        ShowPollution();
+    }
+
+    // Metode til når man samler skrald
+    public void DecreasePollution(int amount)
+    {
+        pollution = pollution - amount;
+        if (pollution < 0)
+        {
+            pollution = 0;
+        }
+        ShowPollution();
+    }
+
+    // Pollution meter visuelt:
+    public void ShowPollution()
+    {
+        int barLength = 20;
+        int filled = pollution * barLength / maxPollution;
+
+        string bar = "";
+        int i = 0;
+        while (i < filled)
+        {
+            bar = bar + "#";
+            i = i + 1;
+        }
+        while (i < barLength)
+        {
+            bar = bar + "-";
+            i = i + 1;
+        }
+
+        int percent = pollution * 100 / maxPollution;
+        Console.WriteLine("Pollution: [" + bar + "] " + pollution + "/" + maxPollution + " (" + percent + "%)");
+    }
+
+    public void Test() {
+        PollutionMeterVisual pollutionmeter = new PollutionMeterVisual(); 
+
+        pollutionmeter.IncreasePollution(10);
+
+        pollutionmeter.DecreasePollution(5);
+
+        pollutionmeter.ShowPollution(); 
+    }
+}
+
+
+
