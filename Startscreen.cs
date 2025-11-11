2ab32de7283e92e61848c8c05ecb49833023b8e4
NoahBR <47004125+Noahr-DK@users.noreply.github.com>
2025-11-11 13:10:09 +0100

    English

--- Startscreen.cs
+++ Startscreen.cs
@@ -11,7 +11,7 @@ class Startscreen
     public void Startinformation()
     {
             Console.ForegroundColor = ConsoleColor.Blue; 
-        Console.WriteLine("Sea Turtles have romead Earths oceans for 150 million years");
+        Console.WriteLine("Sea turtles, have been navigating Earth's oceans since the era of the dinosaurs.");
         Console.WriteLine("Now, at present day, Sea Turtles very existence is threathed by man-made pollution.");
         Console.WriteLine("6 out of 7 turtle species are criticality indangered. It said that only 1 out of every 1000 sea turles makes it to adult hood");
         Console.WriteLine("Now it's up to you. Sea Turtle savoir. Build the Cleaning Machine. Clean the ocean. Restore harmony!");
