547f9a7011166302f5ca59bdd8a0709ecce4f04a
NoahBR <47004125+Noahr-DK@users.noreply.github.com>
2025-11-25 13:28:39 +0100

    Update start screen message for clarity

--- Startscreen.cs
+++ Startscreen.cs
@@ -12,7 +12,7 @@ class Startscreen
         Console.WriteLine("Sea turtles, have been navigating Earth's oceans since the age of the dinosaurs.");
         Console.WriteLine("Currently, the very existence of sea turtles is greatly threatened by man-made pollution.");
         Console.WriteLine("6 of the 7 sea turtle species are critically endangered, and only one in every 1,000 hatchlings survives to adulthood.");
-        Console.WriteLine("Now it's up to you. Sea Turtle savoir. Build the Cleaning Machine. Clean the ocean. Restore harmony quickly, before the pollution levels get out of hand !!!");
+        Console.WriteLine("You are the Sea Turtle Savior, it's up to you to act fast! Clean the ocean and build the Cleaning Machine to restore harmony before the pollution levels gets out of hand!");
         Console.WriteLine();
         Console.ForegroundColor = ConsoleColor.Gray; 
 
