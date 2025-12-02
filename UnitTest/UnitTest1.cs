7435516cf9b3ed5879650723437ab427a3d42235
Emil Nielsen <emil0306dk@gmail.com>
2025-12-02 09:54:40 +0100

    Added folders for unit testing

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using SeaTurtleSavior;
+namespace UnitTests;
+
+public class Tests
+{
+    Inventory inv;
+
+    [SetUp]
+    public void Setup()
+    {
+        inv = new Inventory();
+    }
+
+    [Test]
+    public void Test1()
+    {
+        // First 15 items of trash should return true
+        for (int i = 0 ; i < 15 ; i++)
+        {
+            Assert.That(inv.CollectTrash(new Trash("filter", "plastic", false)), Is.EqualTo(true));
+        }
+        // the 16 item of trash should return false
+        Assert.That(inv.CollectTrash(new Trash("filter", "plastic", false)), Is.EqualTo(false));
+        Assert.Pass();
+    }
+}
