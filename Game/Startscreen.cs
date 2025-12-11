6916aadf5ab348ff212b6c2a8df8c4f9ac6c12ee
NoahBR <47004125+Noahr-DK@users.noreply.github.com>
2025-12-11 13:41:17 +0100

    get st

--- Game/Startscreen.cs
+++ Game/Startscreen.cs
@@ -11,8 +11,8 @@ class Startscreen
         Console.ForegroundColor = ConsoleColor.Cyan; 
         Console.WriteLine("Sea turtles, have been navigating Earth's oceans since the age of the dinosaurs.");
         Console.WriteLine("Currently, the very existence of sea turtles is greatly threatened by man-made pollution.");
-        Console.WriteLine("6 of the 7 sea turtle species are critically endangered, and only one in every 1,000 hatchlings survives to adulthood.");
-        Console.WriteLine("You are the Sea Turtle Savior, it's up to you to act fast! Clean the ocean and build the Cleaning Machine to restore harmony before the pollution levels gets out of hand!");
+        Console.WriteLine("6 of the 7 sea turtle species are critically endangered, and only one in every 1,000 sea turtles survives to adulthood.");
+        Console.WriteLine("You are the Sea Turtle Savior, it's up to you to act fast! Clean the ocean and build the Cleaning Machine to restore harmony before the pollution levels get out of hand!");
         Console.WriteLine();
         Console.ForegroundColor = ConsoleColor.Gray; 
 
